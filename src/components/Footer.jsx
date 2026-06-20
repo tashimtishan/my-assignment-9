d9b6dfadc1376e319baa4d86090d92262159567f
tashimtishan <tashimtishan01611412720@gmail.com>
2026-06-20 16:12:52 +0600

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div>
+           <h2 className="font-bold text-3xl">This is Footer</h2> 
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div>
+            <h2 className="font-bold text-3xl">This is Navbar</h2>
+        </div>
+    );
+};
+
+export default Navbar;
