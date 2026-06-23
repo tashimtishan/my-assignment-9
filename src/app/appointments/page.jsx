9b28c612d1c0af87443e48a3906f2e17cde2b770
tashimtishan <tashimtishan01611412720@gmail.com>
2026-06-24 02:15:17 +0600

    just added metadata appointment page

--- src/app/appointments/page.jsx
+++ src/app/appointments/page.jsx
@@ -2,6 +2,12 @@ import DoctorCard from "@/components/DoctorCard";
 import { getDoctors } from "@/lib/getdoctors";
 import AppointmentsList from "@/components/AppointmentsList";
 import { Label, SearchField } from "@heroui/react";
+
+
+ export const metadata = {
+    title: "All Appointments",
+    description: "Browse all available doctors and book your appointment today.",
+};
 const Appointmentspage = async () => {
     const doctors = await getDoctors();
     
